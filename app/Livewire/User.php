3eade37e16003d423f735da335484673a60c299c
brianajiks123 <brianajiks123@gmail.com>
2024-12-23 04:51:06 +0700

    feat: add user

--- app/Livewire/User.php
+++ app/Livewire/User.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\User as ModelsUser;
 use Livewire\Component;
 
 class User extends Component
 {
+    public $menu_list = "see";
+
+    public function chooseMenu($menu)
+    {
+        $this->menu_list = $menu;
+    }
+
     public function render()
     {
-        return view('livewire.user');
+        return view('livewire.user')->with([
+            "all_user" => ModelsUser::all()
+        ]);
     }
 }
--- resources/views/livewire/user.blade.php
+++ resources/views/livewire/user.blade.php
@@ -1,3 +1,86 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <div class="container">
+        <div class="row my-3">
+            <div class="col">
+                <button class="btn {{ $menu_list == 'see' ? 'btn-primary' : 'btn-outline-primary' }}"
+                    wire:click="chooseMenu('see')">All User</button>
+                <button class="btn {{ $menu_list == 'add' ? 'btn-primary' : 'btn-outline-primary' }}"
+                    wire:click="chooseMenu('add')">Add User</button>
+                <button class="btn btn-info" wire:loading>Loading ...</button>
+            </div>
+        </div>
+
+        {{-- All Functionality --}}
+        <div class="row">
+            <div class="col">
+                @if ($menu_list == 'see')
+                    {{-- See User --}}
+                    <div class="card border-primary">
+                        <div class="card-header">All User</div>
+                        <div class="card-body">
+                            <table class="table table-bordered text-center">
+                                <thead>
+                                    <th>No.</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Verified</th>
+                                    <th>Role</th>
+                                    <th>Action</th>
+                                </thead>
+                                <tbody>
+                                    {{-- Get User From Database --}}
+                                    @foreach ($all_user as $user)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                            <td>
+                                                <span
+                                                    class="badge {{ $user->email_verified_at ? 'bg-success' : 'bg-danger' }}">
+                                                    {{ $user->email_verified_at ? 'Verified' : 'Not Verified' }}
+                                                </span>
+                                            </td>
+                                            <td>{{ $user->role }}</td>
+                                            <td>
+                                                <button
+                                                    class="btn {{ $menu_list == 'edit' ? 'btn-warning' : 'btn-warning' }}"
+                                                    wire:click="chooseMenu('edit')">Edit</button>
+                                                <button
+                                                    class="btn {{ $menu_list == 'delete' ? 'btn-danger' : 'btn-danger' }}"
+                                                    wire:click="chooseMenu('delete')">Delete</button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                @elseif($menu_list == 'add')
+                    {{-- Add User --}}
+                    <div class="card border-primary">
+                        <div class="card-header">Add User</div>
+                        <div class="card-body">
+                            {{--  --}}
+                        </div>
+                    </div>
+                @elseif($menu_list == 'edit')
+                    {{-- Edit User --}}
+                    <div class="card border-primary">
+                        <div class="card-header">Edit User</div>
+                        <div class="card-body">
+                            {{--  --}}
+                        </div>
+                    </div>
+                @elseif($menu_list == 'delete')
+                    {{-- Delete User --}}
+                    <div class="card border-primary">
+                        <div class="card-header">Delete User</div>
+                        <div class="card-body">
+                            {{--  --}}
+                        </div>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
 </div>
